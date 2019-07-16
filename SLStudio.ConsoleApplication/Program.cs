b1f106b9c7c3b925259512beff9c3ec3a9851a10
Adnan Pedroso <adnan_silva54@hotmail.com>
2019-07-16 17:26:29 -0300

    Iniciando o projeto

---
+++ SLStudio.ConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SLStudio.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
