c35d76b874a8a54dc18cc3211ad88a1383a8b595
Adnan Pedroso <adnan_silva54@hotmail.com>
2019-08-11 19:54:06 -0300

    Reset NuGet packages

--- SLStudio/Views/frmMain.Designer.cs
+++ SLStudio/Views/frmMain.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(866, 523);
+            this.Name = "frmMain";
             this.Text = "frmMain";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
