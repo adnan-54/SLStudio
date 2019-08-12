f3c42daba8d2a79a6c566c6e5d51e75724b68edb
wdg <wdg@WDG-DESK-0007.intranet.local>
2019-08-12 14:23:42 -0300

    Start Models

---
+++ SLStudio/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLStudio.Models
+{
+    public class Project
+    {
+        public string projectName;
+        public string projectDescription;
+
+        public Project()
+        {
+        
+        }
+    }
+}
--- SLStudio/Models/Solution.cs
+++ SLStudio/Models/Solution.cs
@@ -6,21 +6,20 @@ using System.Threading.Tasks;
 
 namespace SLStudio.Models
 {
-    public class Solution : IDisposable
+    public class Solution
     {
+        public List<Project> solutionProjects = new List<Project>();
 
+        public string solutionName;
+        public string solutionDescription;
+        public List<string> authors = new List<string>();
 
+        public DateTime solutionCreationDate;
+        public DateTime solutionLastEditDate; 
 
-        #region IDisposable
-        public void Dispose()
+        public Solution()
         {
-            GC.SuppressFinalize(this);
+        
         }
-
-        ~Solution()
-        {
-            Dispose();
-        }
-        #endregion
     }
 }
