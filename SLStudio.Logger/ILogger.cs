26692143acb3815bbb6230ef3b9290b654bb8533
Adnan <adnan_silva54@hotmail.com>
2019-09-11 18:10:57 -0300

    logger

---
+++ SLStudio.Logger/ILogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLStudio.Logger
+{
+    public interface ILogger
+    {
+        void Error(Exception exception);
+        void Error(string message, string title);
+        void Warning(string message, string title);
+        void Info(string message, string title);
+        DataTable GetLog();
+    }
+}
--- SLStudio.Logger/Logger.cs
+++ SLStudio.Logger/Logger.cs
@@ -1,12 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
 
 namespace SLStudio.Logger
 {
-    public class Logger
+    public class Logger: ILogger
     {
+        private readonly string logPath = Path.Combine(Application.StartupPath, "logger");
+
+        private readonly string owner;
+        private bool isInitialized = false;
+
+        public Logger(string owner)
+        {
+            this.owner = owner;
+
+            Initialize();
+        }
+
+        public void Error(Exception exception)
+        {
+            Log($"Message: {exception.Message}\nStackTrace: {exception.StackTrace}\nSource: {exception.Source}", exception.ToString(), LogType.Error);
+        }
+
+        public void Error(string message, string title)
+        {
+            Log(message, message, LogType.Error);
+        }
+
+        public void Info(string message, string title)
+        {
+            Log(message, message, LogType.Info);
+        }
+
+        public void Warning(string message, string title)
+        {
+            Log(message, message, LogType.Warning);
+        }
+
+        public DataTable GetLog()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Log(string message, string title, LogType type)
+        {
+            
+        }
+
+        private void Initialize()
+        {
+            
+
+            isInitialized = true;
+        }
+    }
+
+    enum LogType
+    {
+        Error,
+        Info,
+        Warning
     }
 }
