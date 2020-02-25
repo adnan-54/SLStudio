39d48700fcaebe51cb3e4ef9baf20c7e62f114de
Adnan Pedroso <adnan_silva54@hotmail.com>
2020-02-25 00:59:16 -0300

    wip - checkpoint

--- SLStudio/Core/Modules/StatusBar/IStatusBar.cs
+++ SLStudio/Core/Modules/StatusBar/IStatusBar.cs
@@ -2,5 +2,10 @@
 {
     public interface IStatusBar
     {
+        bool IsBusy { get; set; }
+        string IsBusyStatus { get; set; }
+        string Status { get; set; }
+        int? Line { get; set; }
+        int? Column { get; set; }
     }
 }
--- SLStudio/Properties/AssemblyInfo.cs
+++ SLStudio/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("20.2.174.291")]
-[assembly: AssemblyFileVersion("20.2.174.290")]
+[assembly: AssemblyVersion("20.2.194.311")]
+[assembly: AssemblyFileVersion("20.2.194.310")]
 [assembly: NeutralResourcesLanguage("en")]
