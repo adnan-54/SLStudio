f8ef5a23f55d8c0b3b5cbd8dcf6df4b26db12243
Adnan Pedroso <adnan_silva54@hotmail.com>
2020-03-08 13:46:20 -0300

    refatored splash screen

--- SLStudio.Tests/Properties/AssemblyInfo.cs
+++ SLStudio.Tests/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("20.3.131.132")]
-[assembly: AssemblyFileVersion("20.3.131.132")]
+[assembly: AssemblyVersion("20.3.134.135")]
+[assembly: AssemblyFileVersion("20.3.134.135")]
