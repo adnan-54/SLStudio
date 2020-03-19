ad7c56432b5b941a7d13a9f63bc5f73697608bd6
Adnan Pedroso <adnan_silva54@hotmail.com>
2020-03-19 03:14:30 -0300

    avoid creating multiple loggers for the same type

--- SLStudio.Core/Utilities/LoggingFactory/DefaultLoggingFactory.cs
+++ SLStudio.Core/Utilities/LoggingFactory/DefaultLoggingFactory.cs
@@ -1,4 +1,6 @@
 ﻿using SLStudio.Core.Utilities.Logger;
+using System;
+using System.Collections.Generic;
 
 namespace SLStudio.Core
 {
@@ -6,16 +8,24 @@ namespace SLStudio.Core
     {
         private readonly ILoggingService loggingService;
         private readonly ICommandLineArguments commandLineArguments;
+        private readonly Dictionary<Type, ILogger> logs;
 
         public DefaultLoggingFactory(ILoggingService loggingService, ICommandLineArguments commandLineArguments)
         {
             this.loggingService = loggingService;
             this.commandLineArguments = commandLineArguments;
+            logs = new Dictionary<Type, ILogger>();
         }
 
         public ILogger GetLoggerFor<Type>() where Type : class
         {
-            return new DefaultLogger(typeof(Type), loggingService, commandLineArguments);
+            if (!logs.TryGetValue(typeof(Type), out ILogger logger))
+            {
+                logger = new DefaultLogger(typeof(Type), loggingService, commandLineArguments);
+                logs.Add(typeof(Type), logger);
+            }
+
+            return logger;
         }
     }
 }
