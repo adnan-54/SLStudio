b2abbc13a8fc127da76603e93509d4d4d76b34bb
Adnan Pedroso <adnan_silva54@hotmail.com>
2020-03-19 02:06:47 -0300

    draggable splash screen

---
+++ SLStudio.Core/Behaviors/DraggableWindowBehavior.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.Mvvm.UI.Interactivity;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SLStudio.Core.Behaviors
+{
+    public class DraggableWindowBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.MouseDown += OnMouseDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseDown -= OnMouseDown;
+            base.OnDetaching();
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                AssociatedObject.DragMove();
+        }
+    }
+}
