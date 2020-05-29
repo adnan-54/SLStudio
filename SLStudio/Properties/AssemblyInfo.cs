5bbbd879a8545d6a7f7deb48648b364dd7a8c6b4
Adnan Pedroso <adnan_silva54@hotmail.com>
2020-05-29 09:14:29 -0300

    update handycontrols package

--- SLStudio/Properties/AssemblyInfo.cs
+++ SLStudio/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("20.5.18.917")]
-[assembly: AssemblyFileVersion("20.5.18.917")]
+[assembly: AssemblyVersion("20.5.19.918")]
+[assembly: AssemblyFileVersion("20.5.19.918")]
 [assembly: NeutralResourcesLanguage("en")]
