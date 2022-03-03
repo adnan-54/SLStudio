049b2511c872cab4da627b2bbe6d942690eec022
Adnan Pedroso <adnan_silva54@hotmail.com>
2022-03-02 22:29:13 -0300

    wip logger

---
+++ Client/SLStudio.Logging/Interfaces/ILogManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLStudio.Logging;
+
+public interface ILogManager
+{
+    ILogger CreateLogger(string name);
+}
+
+public interface ILogger
+{
+    string Name { get; }
+
+    void Log(string message, LogLevel level);
+}
+
+public enum LogLevel
+{
+    Debug,
+    Information,
+    Warning,
+    Error,
+    Fatal
+}
+
+public record Log(Guid Id, Guid InstanceId, string Sender, LogLevel Level, string Message, string StackTrace, DateTime Date);
